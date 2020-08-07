8f827ac99af66d5c99fd52261e1279dfdd4ef618
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2020-08-07 15:04:56 +0200

    create and manage the entities

---
+++ src/main/java/com/bookstore/api/entities/Book.java
@@ -0,0 +1,44 @@
+package com.bookstore.api.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+@Data
+@Entity
+@Table(name = "books",schema = "public")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "sku")
+    private String sku;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "unit_price")
+    private BigDecimal unit_price;
+    @Column(name = "image_url")
+    private String image_url;
+    @Column(name = "active")
+    private boolean active;
+    @Column(name = "promotion")
+    private boolean promotion;
+    @Column(name = "selected")
+    private boolean selected;
+    @Column(name = "units_in_stock")
+    private Integer units_in_stock;
+    @Column(name = "date_created")
+    private Date date_created;
+    @Column(name = "last_updated")
+    private Date last_updated;
+
+
+    @ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    @JoinColumn(name = "categories_id",nullable = false)
+    private Category category;
+}
---
+++ src/main/java/com/bookstore/api/entities/Category.java
@@ -0,0 +1,28 @@
+package com.bookstore.api.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "categories",schema = "public")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "category",fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    private Set<Book> bookSet;
+}
