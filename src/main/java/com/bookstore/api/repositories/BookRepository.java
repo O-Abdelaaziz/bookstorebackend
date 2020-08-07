7dbfc017db664c970261f2df00345d2f31234ac4
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2020-08-07 16:51:07 +0200

    add crossorigin annotation to book repository

--- src/main/java/com/bookstore/api/repositories/BookRepository.java
+++ src/main/java/com/bookstore/api/repositories/BookRepository.java
@@ -2,6 +2,8 @@ package com.bookstore.api.repositories;
 
 import com.bookstore.api.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("*")
 public interface BookRepository  extends JpaRepository<Book,Long> {
 }
