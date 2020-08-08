5f518e37a6752e772b9b0e6625ff99445682c372
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2020-08-08 09:14:10 +0200

    add findByNameContaining query

--- src/main/java/com/bookstore/api/repositories/CategoryRepository.java
+++ src/main/java/com/bookstore/api/repositories/CategoryRepository.java
@@ -4,6 +4,6 @@ import com.bookstore.api.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("*")
+//@CrossOrigin("*")
 public interface CategoryRepository  extends JpaRepository<Category,Long> {
 }
